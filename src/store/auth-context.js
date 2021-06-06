30a933bd0fbd2ff36bdb8377b697a370bfb748a4
ngatyah <davidngatyah@gmail.com>
2021-06-06 13:27:47 +0300

    "adding a login context'

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 const AuthContext = React.createContext({
     isLoggedIn: false,
-    onLogout: () => { }
+    onLogout: () => { },
+    onLogin: (email, password) => { }
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const logoutHandler = () => {
+        setIsLoggedIn(false);
+    }
+    const logInHandler = () => {
+        setIsLoggedIn(true);
+    }
+    return <AuthContext.Provider value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: logInHandler
+    }}>{props.children}</AuthContext.Provider>
+}
+
 export default AuthContext;
